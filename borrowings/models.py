ee95f2d9ecd1b72ad68d7c6d583321e78e7e141e
Mykyta Fedotov <nikita.fedotov222@gmail.com>
2024-10-24 14:04:54 +0300

    created borrowing model

--- borrowings/models.py
+++ borrowings/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+from books.models import Book
+from library_service import settings
+
+
+class Borrowing(models.Model):
+    borrow_date = models.DateField(auto_now_add=True)
+    expected_return_date = models.DateField()
+    actual_return_date = models.DateField()
+    book_id = models.OneToOneField(
+        Book,
+        on_delete=models.CASCADE,
+        related_name="borrowing"
+    )
+    user_id = models.OneToOneField(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="borrowing"
+    )
