ad600ba528bdb17e2c0ebca4e5c2c0cefa4af001
Mykyta Fedotov <nikita.fedotov222@gmail.com>
2024-10-24 18:37:42 +0300

    removed unused import

--- borrowings/serializers.py
+++ borrowings/serializers.py
@@ -1,5 +1,4 @@
 from rest_framework import serializers
-from rest_framework.exceptions import ValidationError
 
 from books.serializers import BookSerializer
 from borrowings.models import Borrowing
