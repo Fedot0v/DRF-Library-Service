443c8039052302fe4049032863e40a5981b32ac2
Mykyta Fedotov <nikita.fedotov222@gmail.com>
2024-10-24 18:36:21 +0300

    implemented a view with filtering by user_id for admins and an is_active filter that checks whether the book has been returned

--- borrowings/views.py
+++ borrowings/views.py
@@ -1,3 +1,44 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions
 
-# Create your views here.
+from borrowings.models import Borrowing
+from borrowings.serializers import (
+    BorrowingSerializer,
+    BorrowingListSerializer,
+    BorrowingDetailSerializer
+)
+
+
+class BorrowingViewSet(viewsets.ModelViewSet):
+    queryset = Borrowing.objects.all()
+    serializer_class = BorrowingSerializer
+    permission_classes = (permissions.IsAuthenticated,)
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+    def get_queryset(self):
+        queryset = self.queryset
+
+        if not self.request.user.is_superuser:
+            queryset.filter(user=self.request.user)
+
+        is_active = self.request.query_params.get("is_active", None)
+        user_id = self.request.query_params.get("user_id", None)
+
+        if is_active is not None:
+            if is_active.lower() == "true":
+                queryset = queryset.filter(actual_return_date__isnull=True)
+            elif is_active.lower() == "false":
+                queryset = queryset.filter(actual_return_date__isnull=True)
+
+        if self.request.user.is_superuser and user_id:
+            queryset = queryset.filter(user_id=user_id)
+
+        return queryset
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return BorrowingListSerializer
+        if self.action == "retrieve":
+            return BorrowingDetailSerializer
+        return BorrowingSerializer
