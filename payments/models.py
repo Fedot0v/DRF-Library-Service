5e392a16d5a9a353034826d76759bbb8152371ed
Mykyta Fedotov <nikita.fedotov222@gmail.com>
2024-10-24 14:44:39 +0300

    created payment model

--- payments/models.py
+++ payments/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+
+class Payment(models.Model):
+    class StatusChoices(models.TextChoices):
+        PENDING = "PENDING", "Pending"
+        PAID = "PAID", "Paid"
+
+    class TypeChoices(models.TextChoices):
+        PAYMENT = "PAYMENT", "Payment"
+        FINE = "FINE", "Fine"
+
+    status = models.CharField(
+        choices=StatusChoices.choices,
+        max_length=20,
+    )
+    type = models.CharField(
+        choices=TypeChoices.choices,
+        max_length=20,
+    )
+    session_url = models.URLField()
+    session_id = models.CharField(max_length=255)
+    money_to_pay = models.DecimalField(max_digits=5, decimal_places=2)
