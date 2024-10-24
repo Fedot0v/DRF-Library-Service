5a8ee2e966a4d44156572940acd353337f1da62d
Mykyta Fedotov <nikita.fedotov222@gmail.com>
2024-10-24 18:56:56 +0300

    added urls

---
+++ borrowings/urls.py
@@ -0,0 +1,15 @@
+from rest_framework.routers import DefaultRouter
+from django.urls import path
+
+from borrowings.views import BorrowingViewSet
+
+router = DefaultRouter()
+router.register("borrowings", BorrowingViewSet)
+
+urlpatterns = [
+    path(
+        "borrowings/<int:pk>/return/",
+        BorrowingViewSet.as_view({"post": "return_book"}),
+        name="return-book"
+    )
+] + router.urls
