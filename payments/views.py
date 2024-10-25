8be195264ef300b6e8fabe1ccdab4ea5b084d05e
Mykyta Fedotov <nikita.fedotov222@gmail.com>
2024-10-25 14:14:21 +0300

    added payment views

--- payments/views.py
+++ payments/views.py
@@ -1,3 +1,41 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions, status
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
-# Create your views here.
+from payments.models import Payment
+from payments.serializers import PaymentSerializer, PaymentListSerializer
+
+
+class PaymentViewSet(viewsets.ModelViewSet):
+    queryset = Payment.objects.all()
+    serializer_class = PaymentSerializer
+    permission_classes = (permissions.IsAuthenticated,)
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+    def get_queryset(self):
+        if self.request.user.is_superuser:
+            return self.queryset.all()
+        return self.queryset.filter(user=self.request.user)
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return PaymentListSerializer
+        return PaymentSerializer
+
+    @action(detail=True, methods=["get"])
+    def success(self, request, pk=None):
+        payment = self.get_object()
+        payment.status = Payment.StatusChoices.PAID
+        payment.save()
+        return Response(
+            {"message": "Payment was successfully processed."},
+            status=status.HTTP_200_OK
+        )
+    @action(detail=True, methods=["get"])
+    def cancel(self, request, pk=None):
+        return Response(
+            {"message": "Payment was canceled."},
+            status=status.HTTP_200_OK
+        )
