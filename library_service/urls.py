d4f55abeaf4cf355fcb8a4d8290c764f32cfb085
Mykyta Fedotov <nikita.fedotov222@gmail.com>
2024-10-25 15:34:25 +0300

    included urls

--- library_service/urls.py
+++ library_service/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
+    path("users/", include("users.urls"), namespace="users"),
+    path("borrowings/", include("borrowings.urls"), namespace="borrowings"),
+    path("books/", include("books.urls"), namespace="books"),
+    path("payments/", include("payments.urls"), namespace="payments"),
 ]
