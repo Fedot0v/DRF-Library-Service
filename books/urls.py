d16765dc7eab1fb89c25e1c37459b583e3ffe515
Mykyta Fedotov <nikita.fedotov222@gmail.com>
2024-10-28 19:37:49 +0200

    added app_name

--- books/urls.py
+++ books/urls.py
@@ -2,8 +2,11 @@ from rest_framework.routers import DefaultRouter
 
 from books.views import BookViewSet
 
+
 router = DefaultRouter()
 
 router.register("books", BookViewSet)
+
+app_name = "books"
 
 urlpatterns = router.urls
